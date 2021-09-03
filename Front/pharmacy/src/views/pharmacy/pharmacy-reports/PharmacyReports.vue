f8160267fb477123317517808d930f81ba5d3393
VasilijeBursac <vasilijebursac@gmail.com>
2021-09-03 14:36:19 +0200

    feat[reports]: Report Pharmacy Rating

--- Front/pharmacy/src/views/pharmacy/pharmacy-reports/PharmacyReports.vue
+++ Front/pharmacy/src/views/pharmacy/pharmacy-reports/PharmacyReports.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="container">
-        <h4 class="h4 align-middle my-4">Izveštaji o apoteci</h4>
+    <div class="container mt-4">
+        <!-- <h4 class="h4 align-middle my-4">Izveštaji o apoteci</h4> -->
+        <h5 class="h5 mb-4 text-left">Izveštaji o apoteci</h5>
         <div class="pharmacy-reports-main">
             <b-card no-body>
                 <b-tabs pills card align="center" lazy>
                     <b-tab title="Ocena apoteke" active>
+                        <ReportPharmacyRating :pharmacyId="pharmacyId" />
                     </b-tab>
 
                     <b-tab title="Ocene farmaceuta">    
@@ -35,10 +37,12 @@ export default {
             pharmacy: {}
         }
     },
+
     mounted(){
     },
+
     components: {
-        
+        ReportPharmacyRating: () => import("./ReportPharmacyRating.vue")
     }
 }
 </script>
